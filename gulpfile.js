afeff6adbac98bc0dab25a517b0acd80470028f8
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-01 11:51:34 +0000

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,17 +14,17 @@ gulp.task('deploy', ['build'], shell.task(
     [ 'git add . ',
       'git ci -am "new version"', // commit changes
       //'git push --force origin master' // push changes to gitbook repo
-      'git push origin master' // push changes to gitbook repo
+      'git push origin master' // push changes to github repo
     ]
   )
 );
 
 gulp.task('ghrepo', shell.task( // Open a browser in the github book repo
-  'hub browse etsiiull/gitbook-skeleton'
+  'hub browse'
 ));
 
 gulp.task('apuntes', shell.task( // Open web browser in the github deployment of this book
-  'open https://etsiiull.github.io/gitbook-skeleton'
+  'open https://pal-ull.github.io/'
 ));
 
 gulp.task("pre-install", shell.task([ // global dependencies
